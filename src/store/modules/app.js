6ef1131feaf0381b1366312353f8600c7c962cfa
mahoneming <441096207@qq.com>
2018-01-10 08:57:40 +0800

    no message

--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -1,12 +1,34 @@
+import {
+  login
+} from '@/api/index'
+import {
+  setToken
+} from '@/config/auth'
+import router from '../../router/index'
+
+const LOGIN = 'LOGIN'
+
 const app = {
   state: {
-    
+    phone: '',
+    token: ''
   },
   mutations: {
-    
+    [LOGIN](state, payload) {
+      state.phone = payload.phone
+      setToken(payload.token)
+    }
   },
   actions: {
-    
+    async Login({
+      commit
+    }, payload) {
+      let res = await login(payload.phone, payload.code)
+      if (res.flag) {
+        commit('LOGIN', res.data)
+        router.push('/')
+      }
+    }
   }
 }
 
--- src/views/HelloWorld.vue
+++ src/views/HelloWorld.vue
@@ -6,19 +6,26 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-
+  name: "HelloWorld",
+  data() {
+    return {};
+  },
+  methods: {
+    login() {
+      let data = {
+        phone: this.phone,
+        code: this.code
+      };
+      this.$store.dispatch("Login", data);
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss">
-@import '../styles/common.scss';
-@import '../styles/util.scss';
+@import "../styles/common.scss";
+@import "../styles/util.scss";
 .hello {
   width: rem(20);
 }
