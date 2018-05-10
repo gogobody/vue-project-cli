c863e49659e6a2131fd93fea9385c6c4f7ee90b2
mingh <mingh@meitianiot.com>
2018-05-10 16:25:01 +0800

    no message

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,7 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  BASE_API: '"http://10.10.0.28:28888/"',
+  REDIRECT_URI: '"http://www.meitianiot.com/testServers/unlicensedCar/index.html"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,6 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  BASE_API: '"http://10.10.0.28:28888/"',
+  REDIRECT_URI: '"http://www.meitianiot.com/testServers/unlicensedCar/index.html"'
 }
